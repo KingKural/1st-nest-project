e3c253ef637f24155629dc2fd47b743d7d57eb36
KingKural <86247638+KingKural@users.noreply.github.com>
2022-11-25 11:07:04 -0100

    as

--- src/persons/person.controller.ts
+++ src/persons/person.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
+import { PersonDto } from "./dtos/person.dto";
 import { Person } from "./person.entity";
 import { PersonsService } from "./person.service";
 
